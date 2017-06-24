4688998d7b08e37ef7e468bbb8e1352d6294d1d9
takahashi <takahashi@ipl.cs.uec.ac.jp>
2017-06-24 20:10:19 +0900

    fix config for ddskk

--- inits/10-ddskk.el
+++ inits/10-ddskk.el
@@ -3,6 +3,6 @@
   :bind (("C-x j" . skk-mode))
   :init
   (setq skk-isearch-start-mode 'latin)
-  (setq-default skk-kutouten-type '("．" . "，"))
+  (setq-default skk-kutouten-type 'en)
   (setq skk-dcomp-active t))
 
