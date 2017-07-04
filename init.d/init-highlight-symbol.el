2845df95c3e31cc0b55369a099c0b81a1e7db99e
takahashi <takahashi@ipl.cs.uec.ac.jp>
2017-07-04 20:32:25 +0900

    add config for highlight-symbol

--- init.d/init-highlight-symbol.el
+++ init.d/init-highlight-symbol.el
@@ -5,6 +5,7 @@
   :loader :el-get
   :init
   (setq highlight-symbol-idle-delay 0.5)
-  (add-hook 'prog-mode-hook 'highlight-symbol-mode))
+  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+  (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode))
 
 (provide 'init-highlight-symbol)
