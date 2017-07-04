d34a6fdd859c5054614e8e3b9c40ff4171d5429a
takahashi <takahashi@ipl.cs.uec.ac.jp>
2017-07-04 14:19:14 +0900

    fix config for C

--- init.d/init-c.el
+++ init.d/init-c.el
@@ -1,22 +1,26 @@
 (require 'req-package)
 (require 'el-get)
 
-(req-package c-mode
+(req-package company-c-headers
+  :loader :el-get
+  :require (company)
+  :init
+  (add-to-list 'company-backends 'company-c-headers))
+
+(req-package c++-mode
   :loader :build-in
-  :mode (("\\.h\\'" . c-mode)
-	 ("\\.c\\'" . c-mode))
+  :mode (("\\.h\\'" . c++-mode)
+	 ("\\.c\\'" . c++-mode))
   :init
   (setq c-default-style "k&r")
   (setq indent-tabs-mode nil)
   (setq c-basic-offset 2))
 
-(req-package c-eldoc
-  :loader :el-get
-  :require (c-mode)
-  :init
-  (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
-  (add-hook 'c++-mode-hook 'c-turn-on-eldoc-mode)
-  :config
-  (setq eldoc-idle-delay 0.2))
+;; (req-package c-eldoc
+;;   :loader :el-get
+;;   :init
+;;   (add-hook 'c++-mode-hook 'c-turn-on-eldoc-mode)
+;;   :config
+;;   (setq eldoc-idle-delay 0.2))
 
 (provide 'init-c)
