09287c2a8155300d667f636bb8f24aa3714b424d
takahashi <takahashi@ipl.cs.uec.ac.jp>
2017-07-04 20:29:52 +0900

    add highlight-symbol

---
+++ init.d/init-highlight-symbol.el
@@ -0,0 +1,10 @@
+(require 'req-package)
+(require 'el-get)
+
+(req-package highlight-symbol
+  :loader :el-get
+  :init
+  (setq highlight-symbol-idle-delay 0.5)
+  (add-hook 'prog-mode-hook 'highlight-symbol-mode))
+
+(provide 'init-highlight-symbol)
