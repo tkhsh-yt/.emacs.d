4f61d1cc73789bd689f1c89802e709df4d137d1d
takahashi <takahashi@ipl.cs.uec.ac.jp>
2017-07-08 16:26:18 +0900

    add bind by key-chord

--- init.d/init-key-chord.el
+++ init.d/init-key-chord.el
@@ -5,7 +5,12 @@
 
 (req-package key-chord
   :loader :el-get
+  :chords ((":l" . helm-mini)
+	   (":s" . helm-swoop)
+	   (";S" . helm-multi-swoop-all)
+	   (":y" . helm-show-kill-ring))
   :init
-  (setq key-chord-one-key-delay 0.18))
+  (setq key-chord-one-key-delay 0.18)
+  (setq key-chord-two-keys-delay 0.04))
 
 (provide 'init-chord)
