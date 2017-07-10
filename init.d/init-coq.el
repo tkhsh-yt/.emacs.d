2d90ff47ec34b5a2ed4a6d36519eaedb355b6d96
takahashi <takahashi@ipl.cs.uec.ac.jp>
2017-07-10 20:42:52 +0900

    add keybinds for coq-mode

--- init.d/init-coq.el
+++ init.d/init-coq.el
@@ -1,8 +1,13 @@
 (require 'req-package)
 (require 'el-get)
 
-(req-package proof-general
-  :loader :el-get)
+(req-package proof-site
+  :loader :el-get
+  :require (smartrep)
+  :config
+  (smartrep-define-key coq-mode-map "C-c"
+    '(("C-n" . proof-assert-next-command-interactive)
+      ("C-u" . proof-undo-last-successful-command))))
 
 (req-package company-coq
   :loader :el-get
