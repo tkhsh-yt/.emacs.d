4f2171d88054f8aa34773a13d92e025101a8f348
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-19 17:27:00 +0900

    Fix el-get config

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; el-get
 
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
@@ -12,14 +16,22 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
+;; use-package
+
 (el-get-bundle use-package)
 (require 'use-package)
+
+;; diminish
 
 (el-get-bundle diminish)
 (use-package diminish)
 
+;; init-loader
+
 (el-get-bundle init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
+
+;; Emacs 25 から追加された package-selected-packages を分離
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
