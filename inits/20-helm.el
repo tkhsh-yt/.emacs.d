73bb6917d6dc38ae8ac81d323f30b56632294f6b
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-20 02:06:23 +0900

    Fix wrong config

--- inits/20-helm.el
+++ inits/20-helm.el
@@ -2,19 +2,16 @@
   :diminish (helm-mode)
   :bind (("M-x"  . helm-M-x)
          ("C-x C-f" . helm-find-files)
-         ("C-h b" . helm-descbinds)
          ;; ("M-y" . helm-show-kill-ring)
          )
   :init
   (bind-key "C-c h" helm-command-prefix)
   (unbind-key "C-x c")
   (helm-mode 1)
+  (helm-descbinds-install)
   :config
   (bind-keys :map helm-map
              ("<tab>" . helm-execute-persistent-action)
              ("C-i" . helm-execute-persistent-action)
              ("C-z" . helm-select-action))
-  (setq helm-M-x-fuzzy-match              t
-        helm-split-window-in-side-p       t
-        helm-move-to-line-cycle-in-source t))
-
+  (setq helm-M-x-fuzzy-match t))
