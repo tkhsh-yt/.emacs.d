8142a38888737b5f12d4959147cb2e690bbaeb3c
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-20 02:01:42 +0900

    Diminish highlight-symbol mode

--- inits/20-highlight-symbol.el
+++ inits/20-highlight-symbol.el
@@ -1,4 +1,5 @@
 (use-package highlight-symbol
+  :diminish highlight-symbol-mode
   :init
   (add-hook 'prog-mode-hook 'highlight-symbol-mode)
   (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
