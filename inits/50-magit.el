6687186d31f2196d8de79a69192bbf04dc3a3d51
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-22 07:38:31 +0900

    Add config for magit

--- inits/50-magit.el
+++ inits/50-magit.el
@@ -1,1 +1,5 @@
-(use-package magit)
+(use-package magit
+  :bind (("C-x g" . magit-status)) ;; recommended by magit manual
+  :config
+  ;; magit-status をフルフレームで表示
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
