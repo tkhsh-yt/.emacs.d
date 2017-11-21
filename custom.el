aa7d8a3ce70e9ad28f7e8bb9348560d595860de2
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-22 06:05:48 +0900

    Add elscreen config for daemon

--- custom.el
+++ custom.el
@@ -3,10 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" default)))
  '(helm-ag-base-command "rg --vimgrep --no-heading")
+ '(package-selected-packages (quote (magit-popup)))
  '(projectile-enable-caching t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- inits/10-elscreen.el
+++ inits/10-elscreen.el
@@ -7,7 +7,15 @@
   (setq elscreen-tab-display-kill-screen nil)
   (setq elscreen-tab-display-control nil)
   ;; color
-  (copy-face 'mode-line          'elscreen-tab-current-screen-face)
-  (copy-face 'mode-line-inactive 'elscreen-tab-background-face)
-  (copy-face 'mode-line-inactive 'elscreen-tab-control-face)
-  (copy-face 'mode-line-inactive 'elscreen-tab-other-screen-face))
+  (defun set-elscreen-tab-face ()
+    (copy-face 'mode-line          'elscreen-tab-current-screen-face)
+                  (copy-face 'mode-line-inactive 'elscreen-tab-background-face)
+                  (copy-face 'mode-line-inactive 'elscreen-tab-control-face)
+                  (copy-face 'mode-line-inactive 'elscreen-tab-other-screen-face))
+  ;; for GUI
+  (set-elscreen-tab-face)
+  ;; for GUI with daemon
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (set-elscreen-tab-face)))))
