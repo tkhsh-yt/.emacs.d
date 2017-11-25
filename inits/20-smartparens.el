22ab08ed4ce629160cdf440962e6262b856c91a5
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-25 14:19:28 +0900

    Add smartparents config for Lisp

--- inits/20-smartparens.el
+++ inits/20-smartparens.el
@@ -2,4 +2,8 @@
   :diminish smartparens-mode
   :init
   (smartparens-global-mode t)
-  (show-smartparens-global-mode t))
+  (show-smartparens-global-mode t)
+  :config
+  (sp-with-modes '(lisp-mode lisp-interaction-mode emacs-lisp-mode scheme-mode)
+    (sp-local-pair "'" nil :actions nil)
+    (sp-local-pair "`" nil :actions nil)))
