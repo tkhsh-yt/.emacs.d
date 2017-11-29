b8025e0e28cd27a869be289c462c50e874da5528
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-29 14:47:43 +0900

    Add diff-hl

--- inits/00-package.el
+++ inits/00-package.el
@@ -63,6 +63,8 @@
 
 (el-get-bundle highlight-symbol)
 
+(el-get-bundle diff-hl)
+
 ;; project
 
 (el-get-bundle projectile)
--- inits/20-hilight.el
+++ inits/20-hilight.el
@@ -5,3 +5,8 @@
   (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
   :config
   (setq highlight-symbol-idle-delay 0.5))
+
+(use-package diff-hl
+  :init
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  (global-diff-hl-mode t))
