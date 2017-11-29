62de4c5677bd17d2519dc3e9e6ded9fbc0efb83a
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-11-29 14:48:31 +0900

    Back to zenburn-theme

--- custom.el
+++ custom.el
@@ -5,8 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("31e64af34ba56d5a3e85e4bebefe2fb8d9d431d4244c6e6d95369a643786a40e" "4b207752aa69c0b182c6c3b8e810bbf3afa429ff06f274c8ca52f8df7623eb60" "60668f4b17b8b8780d50976c0788abed190353d21d3371b8f244dd44c103b0ea" "759416a7a5f5cb6b8cb26e6db2cf70026aa2324083a888015ee2cad0320f7f19" "d2c61aa11872e2977a07969f92630a49e30975220a079cd39bec361b773b4eb3" "4a7abcca7cfa2ccdf4d7804f1162dd0353ce766b1277e8ee2ac7ee27bfbb408f" "10e3d04d524c42b71496e6c2e770c8e18b153fcfcc838947094dad8e5aa02cef" default)))
+    ("3b36631f95ebfd9ec35f382249ad861f3b3d51f8bed4882184ec8745deaada28" default)))
  '(emoji-fontset-check-version nil)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(projectile-enable-caching t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- inits/05-theme.el
+++ inits/05-theme.el
@@ -1,5 +1,6 @@
 (use-package doom-themes)
 
 (if window-system
-    (load-theme 'doom-one t)
+    ;; (load-theme 'doom-one t)
+    (load-theme 'zenburn t)
   (load-theme 'zenburn t))
