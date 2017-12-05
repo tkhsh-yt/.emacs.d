52a7d3efa4d6c448d4036592190bb4c3565fd57f
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-12-05 18:59:13 +0900

    Fix linum-mode config on doc-view-mode

--- inits/01-emacs.el
+++ inits/01-emacs.el
@@ -14,6 +14,26 @@
 
 (global-linum-mode t)
 
+(defcustom linum-disabled-modes-list '(doc-view-mode pdf-view-mode)
+  "* List of modes disabled when global linum mode is on"
+  :type '(repeat (sexp :tag "Major mode"))
+  :tag " Major modes where linum is disabled: "
+  :group 'linum)
+
+(defcustom linum-disable-starred-buffers 't
+  "* Disable buffers that have stars in them like *Gnu Emacs*"
+  :type 'boolean
+  :group 'linum)
+
+(defun linum-on ()
+  "* When linum is running globally, disable line number in modes defined in `linum-disabled-modes-list'. Changed by linum-off. Also turns off numbering in starred modes like *scratch*"
+  (unless (or (minibufferp) (member major-mode linum-disabled-modes-list)
+          (and linum-disable-starred-buffers (string-match "*" (buffer-name)))
+          )
+    (linum-mode 1)))
+
+(provide 'setup-linum)
+
 ;; 起動時にメッセージを表示しない
 
 (setq inhibit-startup-message t)
