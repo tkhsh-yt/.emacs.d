7ee403da650111da2387bbfcb5dad14da71262ba
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-12-10 14:52:25 +0900

    Change font

--- inits/04-font.el
+++ inits/04-font.el
@@ -1,8 +1,7 @@
 ;; フォント
 
 (when (eq system-type 'darwin)
-  (set-face-attribute 'default nil :family "Source Han Code JP" :height 160))
+  (set-face-attribute 'default nil :family "Cica" :height 200))
 
 (when (eq system-type 'gnu/linux)
-  (set-face-attribute 'default nil :family "Source Han Code JP" :height 125))
-
+  (set-face-attribute 'default nil :family "Cica" :height 125))
