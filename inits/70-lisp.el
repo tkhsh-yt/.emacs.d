48246ee8d0a99f1f1b7b82c5808a0e381346f286
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-12-12 23:38:51 +0900

    Fix hook

--- inits/70-lisp.el
+++ inits/70-lisp.el
@@ -2,6 +2,6 @@
   :diminish lispy-mode
   :defer t
   :init
-  (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
-  (add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
-  (add-hook 'scheme-mode-hook (lambda () (lispy-mode 1))))
+  (add-hook 'emacs-lisp-mode-hook '(lambda () (lispy-mode 1)))
+  (add-hook 'lisp-mode-hook '(lambda () (lispy-mode 1)))
+  (add-hook 'scheme-mode-hook '(lambda () (lispy-mode 1))))
--- inits/70-org.el
+++ inits/70-org.el
@@ -4,4 +4,4 @@
 (use-package org-bullets
   :defer t
   :init
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (add-hook 'org-mode-hook '(lambda () (org-bullets-mode 1))))
