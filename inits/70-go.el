81d8530b63d49c4a6b43e2543b1c8b378806fe1b
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2017-12-13 15:51:08 +0900

    :art: Fix config for golang

--- inits/70-go.el
+++ inits/70-go.el
@@ -1,5 +1,7 @@
 (use-package go-mode
-  :bind (("M-." . godef-jump)
+  :defer t
+  :bind (:map go-mode-map
+         ("M-." . godef-jump)
          ("M-," . pop-tag-mark))
   :init
   (add-hook 'go-mode-hook '(lambda ()
@@ -12,11 +14,13 @@
                                      tab-mark)))))
 
 (use-package company-go
+  :defert t
   :init
   (add-hook 'go-mode-hook 'company-mode)
   (add-hook 'go-mode-hook '(lambda ()
                              (set (make-local-variable 'company-backends) '(company-go)))))
 
 (use-package go-eldoc
+  :defert t
   :init
   (add-hook 'go-mode-hook 'go-eldoc-setup))
