51f49faba601938510bacdfb8d0ecebc62e0aae5
tkhsh-yt <takahashi@ipl.cs.uec.ac.jp>
2018-08-03 02:05:00 +0900

    Update markdown-mode

--- inits/70-markdown.el
+++ inits/70-markdown.el
@@ -1,3 +1,5 @@
 (use-package markdown-mode
   :defer t
-  :mode (("\\.md?\\'" . markdown-mode)))
+  :mode (("\\.md?\\'" . markdown-mode))
+  :config
+  (set-face-attribute 'markdown-code-face nil :inherit 'default))
